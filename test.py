7b4bc023034ff26c5581fe51404448ff9508c74f
hongt10x <103439067@qq.com>
2021-02-20 09:18:59 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+
+# from protocol_utils.ssh import SSHClient, SSHExecutorError
+#
+# client =
+
+
+# lis = [1, 2,3,4]
+# for i,j in enumerate(lis):
+#     pass
+# print i,j
+#
+# if (i+1) == 4:
+#     print "ok"
+
+# def check(*s):
+#     print s[0],type(s[0])
+#
+#
+# check("lis","che")
+# if isinstance(lis, unicode):
+#     print "ok"
+#
+# a = {"aa":"ssss", "aa1":"ss1"}
+# print a.get("aa")
+# ss = False
+# # if ss:
+# #     print "pass"
+# # else:
+# #     print "fail"
+# if "aa" in a.keys():
+#     print "pass"
+# else:
+#     print "fail"
+
+'''
+import json
+
+s = {"a2": "b","c": 99}
+
+# print s,type(s)
+# s1 = json.loads(s)
+# print s1,type(s1)
+# s2 = json.dumps(s1)
+# print s2,type(s2)
+
+if s.get("c"):
+    print "pass1111"
+'''
+#
+# try:
+#     print(1/1)
+#     # try fail
+# except Exception as e:
+#     print(e)
+#     # try success
+# else:
+#     print "pass"
+# finally:
+#     print 123
+
+# print "ok"
+# try:
+#     with open("main.py","r") as f:
+#         f.read()
+# except:
+#     print "bad"
+#
+# print "成功"
+#
+# print "测试中文"
+
+try:
+    a = 1/0
+except Exception as e:
+    print e
+else:
+    print "fail--->"
+
+
+
+
+
+
+
+
+
+
+
+
+
